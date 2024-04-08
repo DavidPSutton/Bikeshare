27accfa56d15cc9b24bba00314fb3c7220f67905
DavidPSutton <suttondavidp@gmail.com>
2024-04-08 17:03:11 -0400

    markdown upload

--- Queries/Cyclistic Start Station by User Type.sql
+++ Queries/Cyclistic Start Station by User Type.sql
@@ -2,28 +2,21 @@
 In order to see which 10 starting stations were most popluar I ran a query to find the most common NOT NULL "casual" user station...
 */
 
-	WITH StartingStations AS (
-		SELECT start_station_name, COUNT(start_station_name) AS station_count, user_type
-		FROM TripData_2023
-		WHERE start_station_name IS NOT NULL
-		GROUP BY start_station_name, user_type
-			)
-	SELECT TOP 10 MAX(station_count) as count_times_used, start_station_name, user_type
-	FROM StartingStations
-	WHERE user_type = 'casual'
-	GROUP BY user_type, start_station_name
-	ORDER BY count_times_used DESC
+SELECT TOP 10 user_type, start_station_name, COUNT(start_station_name) AS station_count
+FROM TripData_2023
+WHERE start_station_name IS NOT NULL
+	AND user_type = 'casual'
+GROUP BY start_station_name, user_type
+ORDER BY station_count DESC
 /*
 ...then the 10 most common NOT NULL "member" user station.  
 */
-    WITH StartingStations AS (
-		SELECT start_station_name, COUNT(start_station_name) AS station_count, user_type
-		FROM TripData_2023
-		WHERE start_station_name IS NOT NULL
-		GROUP BY start_station_name, user_type
-			)
-	SELECT TOP 10 MAX(station_count) as count_times_used, start_station_name, user_type
-	FROM StartingStations
-	WHERE user_type = 'member'
-	GROUP BY user_type, start_station_name
-	ORDER BY count_times_used DESC;
+ 
+SELECT TOP 10 user_type, start_station_name, COUNT(start_station_name) AS station_count
+FROM TripData_2023
+WHERE start_station_name IS NOT NULL
+	AND user_type = 'member'
+GROUP BY start_station_name, user_type
+ORDER BY station_count DESC
+
+
